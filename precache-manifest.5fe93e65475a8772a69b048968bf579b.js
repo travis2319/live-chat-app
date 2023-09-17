66db573f72fb0a7e5c41b8094345acbaf451ebf5
travis fernandes <travisfernandes2327@gmail.com>
2023-09-17 14:00:57 +0530

    Updates

---
+++ precache-manifest.5fe93e65475a8772a69b048968bf579b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47464464e2512a5acad9",
+    "url": "/live-chat-app/static/css/main.a170fe26.chunk.css"
+  },
+  {
+    "revision": "47464464e2512a5acad9",
+    "url": "/live-chat-app/static/js/main.47464464.chunk.js"
+  },
+  {
+    "revision": "f7c0363e7921f6feb822",
+    "url": "/live-chat-app/static/js/1.f7c0363e.chunk.js"
+  },
+  {
+    "revision": "f6ba9dff30936247ca89",
+    "url": "/live-chat-app/static/js/runtime~main.f6ba9dff.js"
+  },
+  {
+    "revision": "9af329cf563026025580a584bdfb131f",
+    "url": "/live-chat-app/index.html"
+  }
+];
